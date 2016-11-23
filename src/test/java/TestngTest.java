088437b93fe6bc5ef5b8850f25a5fcbc58b9a8bf
Gravituss <zaretskyyy@gmail.com>
2016-11-23 17:29:48 +0200

    first commit

---
+++ src/test/java/TestngTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+
+
+
+public class TestngTest {
+
+    @Test(alwaysRun = true)
+    public void test1(){
+        System.out.println("55455");
+
+
+
+    }
+}
