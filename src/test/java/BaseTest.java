c52dec1f59e5278680f74602e1387644194a6a8c
Gravituss <zaretskyyy@gmail.com>
2016-11-24 18:51:33 +0200

    one more commit

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,7 +2,11 @@ import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import utils.Config;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
@@ -17,13 +21,18 @@ public class BaseTest {
         return new ChromeDriver();
     }
 
+    @AfterMethod
     public void tearDown () {
         driver.quit();
     }
 
-    @Before
+    @BeforeMethod
     public void preConfig () {
         System.setProperty("webdriver.gecko.driver", "C:\\pr\\gecko\\geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "C:\\pr\\cd\\chromedriver.exe");
+        driver = getDriver();
+        driver.manage().timeouts().implicitlyWait(Integer.parseInt(TIMEOUT), TimeUnit.SECONDS);
+        driver.get(BaseTest.URL);
+        driver.manage().window().maximize();
     }
 }
--- src/test/java/TestngTest.java
+++ src/test/java/TestngTest.java
@@ -1,37 +1,25 @@
-
-import org.junit.Ignore;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import pages.BaseRozetkaPage;
 
-import java.util.concurrent.TimeUnit;
+import static org.testng.Assert.assertTrue;
 
 
 public class TestngTest extends BaseTest {
 
-    public void preconditions(){
-        preConfig();
-        driver = getDriver();
-        driver.manage().timeouts().implicitlyWait(Integer.parseInt(TIMEOUT), TimeUnit.SECONDS);
-        driver.get(BaseTest.URL);
-        driver.manage().window().maximize();
-
-    }
 
     @Test
     public void test1() {
-        preconditions();
         BaseRozetkaPage basePage = new BaseRozetkaPage(driver);
         basePage.getDismissBtn().click();
         basePage.getMenuElement("Доставка").click();
-        assert(driver.getPageSource().contains("Доставка производится до входа"));
+        assertTrue(driver.findElement(By.xpath(".//*[@id='deliveries-payments-container']/section[1]/ul/li[2]")).getText().contains("Доставка производится до входа"));
 
-        tearDown();
     }
 
     @Test
     public void test2() {
 
-        preconditions();
         BaseRozetkaPage basePage = new BaseRozetkaPage(driver);
         basePage.getDismissBtn().click();
 
@@ -39,8 +27,7 @@ public class TestngTest extends BaseTest {
         basePage.getCategoriesDropdown().click();
         basePage.getCategorySportItem().click();
         basePage.getSearchField().submit();
-        assert(driver.getPageSource().contains("найдено более"));
+        assertTrue(driver.getPageSource().contains("найдено более"));
 
-        tearDown();
     }
 }
